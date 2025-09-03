1298dfb9a4e1f90b82d4905359ae06c812b9fc43
Kiran Gowda <kiranashgowda007@gmail.com>
2025-09-03 10:19:26 +0530

    Update ingestion.py

--- api/ingestion.py
+++ api/ingestion.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # PostgreSQL connection
-engine = create_engine("postgresql+psycopg2://postgres:GOwda%407924@localhost:5432/detection")
+engine = create_engine("postgresql+psycopg2://postgres:add_password@localhost:5432/detection")
 
 # Load all data from transactions table
 query = "SELECT * FROM transactions LIMIT 1000;"  # use LIMIT to avoid loading full dataset
@@ -10,3 +10,4 @@ df = pd.read_sql(query, engine)
 
 print(df.head())  # preview the first rows
 print(f"Shape of data loaded: {df.shape}")
+

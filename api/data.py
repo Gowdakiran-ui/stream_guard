76bd518d91f446cd06deba3944dfbdcc48e4f002
Kiran Gowda <kiranashgowda007@gmail.com>
2025-09-03 10:18:54 +0530

    Update data.py

--- api/data.py
+++ api/data.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # PostgreSQL connection
-engine = create_engine("postgresql+psycopg2://postgres:GOwda%407924@localhost:5432/detection")
+engine = create_engine("postgresql+psycopg2://postgres:add_password@localhost:5432/detection")
 
 # Correct absolute path
 file_path = r"C:\Users\Kiran gowda.A\Downloads\ieee-fraud-detection\train_transaction.csv"
@@ -11,3 +11,4 @@ file_path = r"C:\Users\Kiran gowda.A\Downloads\ieee-fraud-detection\train_transa
 chunksize = 100000
 for chunk in pd.read_csv(file_path, chunksize=chunksize):
     chunk.to_sql("transactions", engine, if_exists="append", index=False)
+
